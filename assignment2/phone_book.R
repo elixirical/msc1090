848a3a76c327871c89dbf5ef4b1087345dc8cede
elixirical <alvinhan1999@gmail.com>
2022-09-26 18:18:31 -0400

    assignment 2

---
+++ assignment2/phone_book.R
@@ -0,0 +1,6 @@
+# Name: Alvin Han
+# SciNet username: tmp_ahan
+# Description:
+#   script prints phone book contacts filtered out by the specified province
+
+names <- c("Alice", "John", "Mike", "Erik", "Frank", "Charlotte")
