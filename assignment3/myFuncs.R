1beec83060e8279f8353ce3282de1ee110252c2f
elixirical <alvinhan1999@gmail.com>
2022-09-29 17:20:14 -0400

    um

---
+++ assignment3/myFuncs.R
@@ -0,0 +1,15 @@
+geom.pos.mean <- function(x) {
+#  numValues <- 0
+#  product <- 1
+#  for (value in x) {
+#    if (x > 0) {
+#      numValues <- numValues + 1
+#      product <- prod(product, x)
+#    }
+#  }
+#  return(product**(1 / numValues))
+#}
+
+
+
+neg.mean <- function
