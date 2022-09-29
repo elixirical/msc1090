a7ded18e2aee4f9d282f23e6872d086fc6f23279
elixirical <alvinhan1999@gmail.com>
2022-09-29 19:50:42 -0400

    completed assignment3

--- assignment3/myFuncs.R
+++ assignment3/myFuncs.R
@@ -1,20 +1,28 @@
+# Name: Alvin Han
+# SciNet username: tmp_ahan
+# Description:
+#   Various functions that calculate the geometric mean of positive numbers,
+#   the arithmetic men of negative numbers, and the harmonic means of all
+#   non-0 numbers.
+
 geom.pos.mean <- function(x) {
-  subList <- x[(x > 0)]
-  product <- prod(subList)
-  numPositive <- length(subList)
-  return(product**(1 / numPositive))
+  subList <- x[(x > 0)]             #creates a vector composed of values that are greater than 0
+  product <- prod(subList)          #calculates the product of all the + values
+  numPositive <- length(subList)    #grabs the count of all + values
+  return(product**(1 / numPositive))#returns the geometric positive mean
 }
 
 neg.mean <- function(x) {
-  subList <- x[(x < 0)]
-  sumOfNeg <- sum(subList)
-  numNegative <- length(subList)
-  return(sumOfNeg / numNegative)
+  subList <- x[(x < 0)]             #creates a vector composed of values less than 0
+  sumOfNeg <- sum(subList)          #calculates the sum of all - numbers
+  numNegative <- length(subList)    #grabs the count of all - numbers
+  return(sumOfNeg / numNegative)    #returns the mean of all - numbers
 }
 
 harmonicMean <- function(x) {
-  subList <- x[(x != 0)]
-  inverseList <- 1 / x
-  listLength <- length(inverseList)
-  denominator <- sum(inverseList)
+  subList <- x[(x != 0)]            #creates a vector composed of values that are not 0
+  inverseList <- 1 / subList        #creates a vector composed of the inverse values of subList
+  listLength <- length(inverseList) #grabs the count of all non-0 values
+  denominator <- sum(inverseList)   #sums up the inverse values
+  return (listLength / denominator) #divides the count by the sum of the inverse values
 }
