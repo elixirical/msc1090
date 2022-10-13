ec784365b665cda4508e7928dba6527722749960
Alvin Han <alvinhan1999@gmail.com>
2022-10-13 19:21:53 -0400

    finished max.angular.diff; working on sim.null.hypo

--- assignment5/Circle.Utilities.R
+++ assignment5/Circle.Utilities.R
@@ -1,3 +1,28 @@
-max.angular.diff <- function(x) {
+calc.angular.diff <- function(x, y) {
+  if (y < x) {
+    return(pi*2 - (x-y))
+  } else return(y-x)
+}
 
+max.angular.diff <- function(x) {
+  sorted.angles <- sort(x, decreasing = FALSE)
+  angular.differences <- c()
+  for (n in 2:length(sorted.angles)) {
+    #print(angular.differences)
+    angular.differences <- append(angular.differences,
+                                 calc.angular.diff(sorted.angles[n-1],
+                                                   sorted.angles[n]))
+  }
+  angular.differences <- append(angular.differences,
+                                calc.angular.diff(sorted.angles[length(sorted.angles)],
+                                                  sorted.angles[1]))
+  #print(angular.differences)
+  return(max(angular.differences))
+}
+
+sim.null.hypo <- function(k, n) {
+  n.vector <- sample(c(k), n, replace = TRUE)
+  n.vector <- sapply(n.vector, runif, 0, 2*pi)
+  n.vector <- sapply(n.vector, max.angular.diff)
+  return(n.vector)
 }
