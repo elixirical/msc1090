f6f6f7799507ae4945d04a3059cf7ff19a8580a5
Alvin Han <alvinhan1999@gmail.com>
2022-10-17 13:00:09 -0400

    frankly idk what im doing and I assume that the stuff for the second file will be covered this week so idk why im even bothering tearing my hair out rn

--- assignment5/test.bird.data.R
+++ assignment5/test.bird.data.R
@@ -1,11 +1,25 @@
 source("Circle.Utilities.R")
-
+library("EnvStats")
 
 arg <- commandArgs(trailingOnly = TRUE)
-if (length(arg) != 0) {
+if (length(arg) != 1) {
   cat("Please supply a single numerical argument following the command! \n",
-      "Example: Rscript test.bird.data.R 15")
+      "Example: Rscript test.bird.data.R 15\n")
   quit()
-} else cat("Testing our hypothesis with", arg[1], " birds in each batch!")
+} else cat("Testing our hypothesis with", as.numeric(arg[1]), "birds in each batch!\n")
 
-my.data <- rtri(arg[1], 0.5 * pi, 1.5 * pi, mode = pi)
+num.birds <- as.numeric(arg[1])
+
+generate.data <- function(k) {
+  my.data <- rtri(k, 0.5 * pi, 1.5 * pi, mode = pi)
+}
+
+#test<-c(1,2,3,2,4,1,5,6)
+#hist(test)
+
+simulated.null.hypo <- sim.null.hypo(num.birds,10000)
+#print(simulated.null.hypo)
+#hist(simulated.null.hypo, breaks=21, freq=FALSE)
+
+cdf.null.hypo <- calc.cdf(simulated.null.hypo)
+print(cdf.null.hypo)
