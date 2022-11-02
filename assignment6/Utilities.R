d168749728e6815eea72e58362317c3bbede05e0
Alvin Han <alvinhan1999@gmail.com>
2022-11-02 18:08:22 -0400

    quadratic modelling fns done

--- assignment6/Utilities.R
+++ assignment6/Utilities.R
@@ -12,13 +12,27 @@ build.digit.freqs <- function(x, y) {
   no.zero.pops <- gsub(",","",no.zero.pops)
   no.zero.pops <- no.zero.pops[!is.na(as.numeric(no.zero.pops))]
   to.return <- data.frame(digits=c(),counts=c())
-  print(to.return)
-  for (z in c("1","2","3","4","5","6","7","8","9")) {
-    digit.freq <- length(no.zero.pops[substr(no.zero.pops,1,1) == z])
+  for (z in c(1:9)) {
+    digit.freq <- length(no.zero.pops[substr(no.zero.pops,1,1) == toString(z)])
     to.return <- rbind(to.return, data.frame(digits=c(z),counts=c(digit.freq)))
   }
   return(to.return)
 }
 
-#test <- build.digit.freqs(load.data("1710014201-eng.csv"),"X2018")
-#print(test)
+quadratic.model <- function(my.data) {
+  return(lm(formula = counts ~ poly(digits, 2), data = my.data))
+}
+
+plot.data.w.quad.model <- function(my.data, data.model) {
+  #model <- lm(formula = counts ~ poly(digits, 2), data = my.data)
+  plot(my.data$digits, my.data$counts)
+  xx <- seq(min(my.data$digits), max(my.data$counts), len = 100)
+  yy <- data.model$coef %*% rbind(1,xx,xx*xx)
+  lines(xx, yy, lwd = 2, col = "red")
+}
+
+test <- build.digit.freqs(load.data("1710014201-eng.csv"),"X2017")
+print(test)
+test2 <- quadratic.model(test)
+print(summary(test2))
+plot.data.w.quad.model(test,test2)
