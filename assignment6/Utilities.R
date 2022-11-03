106fc54fe1d8308ad7dea02102a33c5adde05d52
Alvin Han <alvinhan1999@gmail.com>
2022-11-03 15:28:12 -0400

    utilities.R finished; lgm works i think even tho it doesnt look like a great fit, and chisquare test works

--- assignment6/Utilities.R
+++ assignment6/Utilities.R
@@ -3,9 +3,25 @@ load.data <- function(x) {
   return(read.csv(x))
 }
 
+benfords.law <- function(x) {
+  return(log10((x+1)/x))
+}
+
+benfords.law.seq <- function(x) {
+  if (x > 1) {
+    return( c(benfords.law.seq(x-1), benfords.law(x)) )
+  } else if (x == 1) {
+    return( benfords.law(1) )
+  }
+}
+
+benfords.law.df <- function() {
+  return(data.frame( digits=c(1:9), freq=benfords.law.seq(9)))
+}
+
 # function which takes above data as data frame, a year, as arguments
 # then calculates the frequency of each possible first digit and returns
-# the digits and the frequency in a dataframe -> no towns with 0 pop
+# the digits and the frequency indigits_squared a dataframe -> no towns with 0 pop
 build.digit.freqs <- function(x, y) {
   pops.from.year <- x[y]
   no.zero.pops <- pops.from.year[pops.from.year != 0]
@@ -19,49 +35,74 @@ build.digit.freqs <- function(x, y) {
   return(to.return)
 }
 
+#digit.freqdigits_squared.proportions <- function(x) {
+#  total <- sum(x$counts)
+#  return(datadigits_squared.frame(digits=c(1:9),freq=x$counts/total))
+#}
+
 quadratic.model <- function(my.data) {
-  return(lm(formula = counts ~ poly(digits, 2), data = my.data))
+  #return(lm(formula = counts ~ poly(digits, 2), data = my.data))
+  digits_squared <- my.data$digits**2
+  return(lm(formula = counts ~ digits + digits_squared, data = my.data))
 }
 
 plot.data.w.quad.model <- function(my.data, data.model) {
   #model <- lm(formula = counts ~ poly(digits, 2), data = my.data)
   plot(my.data$digits, my.data$counts)
-  xx <- seq(min(my.data$digits), max(my.data$counts), len = 100)
+  xx <- seq(min(my.data$digits), max(my.data$digits), len = 100)
   yy <- data.model$coef %*% rbind(1,xx,xx*xx)
   lines(xx, yy, lwd = 2, col = "red")
 }
 
 generalized.linear.model <- function(my.data) {
-  glmodel <- glm(digits ~ counts, data = my.data, family = gaussian(link = "log"))
+  glmodel <- glm(counts ~ digits, data = my.data, family = poisson(link = "log"))
   return(glmodel)
 }
 
 plot.data.with.glm <- function(my.data, data.model) {
   plot(my.data$digits, my.data$counts)
-  sorted.counts <- sort(my.data$counts)
+  sorted.digits <- sort(my.data$digits)
   #prediction <- predict(data.model, data.frame(counts = sorted.counts), type="response")
   #lines(sorted.counts,prediction)
-  lines(sorted.counts,
+  lines(sorted.digits,
         predict(data.model,
-                data.frame(counts = sorted.counts),
-                type = "response"))
+                data = data.frame(counts = sorted.digits),
+                type = "response"),
+        lwd = 2,
+        col = "red")
 }
 
-test <- build.digit.freqs(load.data("1710014201-eng.csv"),"X2017")
-print(test)
-test2 <- quadratic.model(test)
-print(summary(test2))
-plot.data.w.quad.model(test,test2)
+benfords.chisq.test <- function(x) {
+  return(chisq.test(x$counts, p = benfords.law.df()$freq))
+}
 
-plot(test2$residuals)
-plot(test$digits,test2$residuals)
-plot(test$counts,test2$residuals)
+chisq.p.value.message <- function(x) {
+  if (x$p.value < 0.05) {
+    cat("The null hypothesis that the data follows Benford's law is rejected, with a p value of", x$p.value, "\n")
+  } else cat("The null hypothesis that the data does not follow Benford's law cannot be rejected, with a p value of", x$p.value, "\n")
+}
+
+#test <- build.digit.freqs(load.data("1710014201-eng.csv"),"X2017")
+#print(test)
+#test2 <- quadratic.model(test)
+#print(summary(test2))
+#plot.data.w.quad.model(test,test2)
+
+#plot(test2$residuals)
+#plot(test$digits,test2$residuals)
+#plot(test$counts,test2$residuals)
 #par(mfrow = c(1,1))
-hist(test2$residuals, breaks=11)
-qqnorm(test2$residuals)
-qqline(test2$residuals)
+#hist(test2$residuals, breaks=11)
+#qqnorm(test2$residuals)
+#qqline(test2$residuals)
 
+#test3 <- generalized.linear.model(test)
+#print(summary(test3))
+#plot.data.with.glm(test,test3)
+#print(benfords.law.df())
+#print(digit.freq.proportions(test))
+#print(benfords.chisq.test(test)$p.value)
 
-test3 <- generalized.linear.model(test)
-print(summary(test3))
-plot.data.with.glm(test,test3)
+#print(benfords.chisq.test(test))
+#chisq.p.value.message(benfords.chisq.test(test))
+#print(chisq.test(test$counts, p=benfords.law.df()$freq))
