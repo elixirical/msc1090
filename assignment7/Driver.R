d1171f97dc57ea1912f5c32aa6b9f1698545ecc2
Alvin Han <alvinhan1999@gmail.com>
2022-11-10 12:28:56 -0800

    everything is done !

--- assignment7/Driver.R
+++ assignment7/Driver.R
@@ -1,8 +1,14 @@
+# loads Utilities.R, which contains all the functions needed
 source("Utilities.R")
 
+# grabs any trailing command line arguments
 arg = commandArgs(trailingOnly = TRUE)
 
+# one function to check the arguments, then generate a graph of the bootstrap averages + BCa intervals
 driver.fn <- function(x) {
-  check.args(x)
-  plot.stuff(birds(k, 1000, 2000))
+  check.args(arg)
+  plot.stuff(birds(x, 1000, 2000))
 }
+
+# runs the driver function
+suppressWarnings(driver.fn(arg[1]))
--- assignment7/Utilities.R
+++ assignment7/Utilities.R
@@ -5,15 +5,15 @@ library(plotrix)
 # checks to make sure that a single numeric argument is passed to the command
 check.args <- function(x) {
   if ((length(x) != 1) || is.na(as.numeric(x[1]))) {
-    cat("Please supply a single numeric value k following the scriptname.")
-    cat("Example: Rscript Driver.R 5")
+    cat("Please supply a single numeric value k following the scriptname.\n")
+    cat("Example: Rscript Driver.R 5\n")
     quit()
   }
 }
 
 # calculates the mode of a given vector of numbers
 calculate.mode <- function(x) {
-  hist.of.vector <- hist(x, breaks=40, plot = FALSE) # generates a histogram using the vector
+  hist.of.vector <- hist(x, breaks=51, plot = FALSE) # generates a histogram using the vector
   largest.density <- sort(hist.of.vector$density, decreasing = TRUE)[1] # creates a sorted list of the greatist density, and grabs the first (ie. largest) value
   index.of.largest.density <- match(largest.density,hist.of.vector$density) #matches the largest density with the original sequence to find its index
   return(hist.of.vector$mids[index.of.largest.density]) # returns the mid point at that index
@@ -29,21 +29,21 @@ non.para.bootstrapping <- function(x, n) {
   return(boot(data = x, statistic = my.mode, R = n))
 }
 
-# ``
-birds <- function(k, m, n) { # three integers
-  bootstrap.t0.bca <- data.frame(k=c(), t=c(), lower=c(), upper=c())
-  for (z in 5:k) {
-    #print(z)
-    distribution <- sim.null.hypo(z, m)
-    bootstrap <- non.para.bootstrapping(distribution, n)
-    #print(summary(bootstrap))
-    bootstrap.bca.ci <- boot.ci(bootstrap)$bca[4:5]
-    bootstrap.t0.bca <- rbind(bootstrap.t0.bca, data.frame(k=c(z), t=c(bootstrap$t0), lower=c(bootstrap.bca.ci[1]), upper=c(bootstrap.bca.ci[2])))
-    #bootstrap.t0.bca[[z]] <- list(bootstrap$t0, bootstrap.bca.ci)
+# this function takes in functions from Circle.Utilities.R and uses them to generate the max angular
+# distributions for samples from
+birds <- function(k, m, n) { # k = number of birds per sample, m = number of times to sample, n = boostraps
+  bootstrap.t.bca <- data.frame(k=c(), t=c(), lower=c(), upper=c()) #inits a data frame for storing t mean + lower and upper CIs
+  for (z in 2:k) { #for each valid possible number of birds per sample, up to k...
+    distribution <- sim.null.hypo(z, m) # generate the max angular distribution
+    bootstrap <- non.para.bootstrapping(distribution, n) # run boostraps on this distribution
+    bootstrap.bca.ci <- boot.ci(bootstrap)$bca[4:5] # calculate the BCa intervals
+    bootstrap.t.bca <- rbind(bootstrap.t.bca, data.frame(k=c(z), t=c(mean(bootstrap$t)), lower=c(bootstrap.bca.ci[1]), upper=c(bootstrap.bca.ci[2]))) #adds to data frame
   }
-  return(bootstrap.t0.bca)
+  print(bootstrap.t.bca)
+  return(bootstrap.t.bca)
 }
 
+# plots the distribution using plotrix
 plot.stuff <- function(x) {
   plotCI(x$k, x$t, x$upper-x$t, x$t-x$lower)
 }
