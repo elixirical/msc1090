44cda1b4db3b7756b5298fb4083258584023f117
Alvin Han <alvinhan1999@gmail.com>
2022-11-23 15:47:37 -0500

    summary of groups done

--- assignment9/plottingTools.R
+++ assignment9/plottingTools.R
@@ -21,30 +21,37 @@ load.file <- function(filename) {
 #}
 
 data.summary <- function(rawdata) {
-  #columns = colnames(rawdata)
-  #print(columns)
-  #means = apply(as.numeric(rawdata), 2, mean, na.rm = TRUE)
-  #print(summary(rawdata)$mean)
-  #means <- colMeans(rawdata)
-  #print(means)
-  #stddevs <- apply(rawdata, 2, sd)
-  #print(stddevs)
-  #return(data.frame(mean=means, stddev=stddevs))
 
   injury.types <- unique(rawdata$injury)
-  #test.groups <- unique(rawdata$)
   print(injury.types)
 
-  #print(rawdata[rawdata$group == 0, ])
+  temp <- data.frame(group=c(),
+                     mean.cell1=c(), sd.cell1=c(),
+                     mean.cov1=c(),  sd.cov1=c(),
+                     mean.cell2=c(), sd.cell2=c(),
+                     mean.cov2=c(),  sd.cov2=c())
+  #temp <- data.frame(group=c())
 
   for (x in injury.types) {
-    print(x)
-    print(rawdata[rawdata$injury == x, ])
+    subset <- rawdata[rawdata$injury == x, ]
 
-    
+    mean1.cell <- mean(subset$layer1.iba.count)
+    sd1.cell <- sd(subset$layer1.iba.count)
+    mean1.cov <- mean(subset$layer1.iba.coverage)
+    sd1.cov <- sd(subset$layer1.iba.coverage)
+    mean2.cell <- mean(subset$layer2.iba.count)
+    sd2.cell <- sd(subset$layer2.iba.count)
+    mean2.cov <- mean(subset$layer2.iba.coverage)
+    sd2.cov <- sd(subset$layer2.iba.coverage)
+
+    temp <- rbind(temp, data.frame(group=c(x),
+                                   mean.cell1=c(mean1.cell), sd.cell1=c(sd1.cell),
+                                   mean.cov1=c(mean1.cov),   sd.cov1=c(sd1.cov),
+                                   mean.cell2=c(mean2.cell), sd.cell2=c(sd2.cell),
+                                   mean.cov2=c(mean2.cov),   sd.cov2=c(sd2.cov)))
   }
 
-  return(TRUE)
+  return(temp)
 }
 
 #gen.plottable.data <- function(x) {
