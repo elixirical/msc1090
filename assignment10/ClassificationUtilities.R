406c7124ff251f35cd08848f90cda6e949bc6f48
Alvin Han <alvinhan1999@gmail.com>
2022-11-28 14:45:05 -0500

    first file read properly; unsure if X1, X2, X3, .. etc are valid names

--- assignment10/ClassificationUtilities.R
+++ assignment10/ClassificationUtilities.R
@@ -1,5 +1,9 @@
 library(caret)
 
+valid.input <- function() {
+
+}
+
 # loads a file from a directory or URL; returns a list of data.frames: [training data, test data]
 load.file <- function(filename) {
   # if (file.exists(filename)) {
@@ -10,53 +14,63 @@ load.file <- function(filename) {
   #  quit()
   #}
 
+  rawdata$label <- sapply(rawdata$label, toString)
   split <- sample(c(T,F), nrow(rawdata), replace = T, prob = c(0.7, 0.3))
 
   return(list(rawdata[split,], rawdata[!split,]))
 }
 
-cv <- function() {
-  return(trainControl(method = "cv", number = 10, classProbs = TRUE))
-}
+#cv <- function() {
+#  return(trainControl(method = "cv", number = 10, classProbs = TRUE))
+#}
 
-dt.model <- function(cv.model, traindata) {
-  return(train(label ~ ., data = traindata, method = "rpart", trControl = cv.model))
-}
+#dt.model <- function(cv.model, traindata) {
+#  return(train(label ~ ., data = traindata, method = "rpart", trControl = cv.model))
+#}
 
-svm.model <- function(cv.model, traindata) {
-  return(train(label ~ ., data = traindata, method = "svmLinear", trControl = cv.model, preProc=c("center","scale")))
-}
+#svm.model <- function(cv.model, traindata) {
+#  return(train(label ~ ., data = traindata, method = "svmLinear", trControl = cv.model, preProc=c("center","scale")))
+#}
 
 data.model <- function(traindata, model) {
 
-  fitControl <- cv()
+  #fitControl <- cv()
+
+  fitControl <- trainControl(method = "cv", number = 10, classProbs = FALSE)
 
   if (model == "DT") {
-    return(dt.model(fitControl, traindata))
+    dt.model <- train(make.names(label) ~ ., data = traindata, method = "rpart", trControl = fitControl)
+    return(dt.model)
+    #return(dt.model(fitControl, traindata))
   } else if (model == "SVM") {
-    return(svm.model(fitControl, traindata))
+    svm.model <- train(make.names(label) ~ ., data = traindata, method = "svmLinear", trControl = fitControl, preProc=c("center","scale"))
+    return(svm.model)
+    #return(svm.model(fitControl, traindata))
   }
 }
 
 kNN.model <- function(traindata) {
-  knnFit <- train(label ~ ., data = traindata, method = 'knn', preProcess = c('center', 'scale'), tuneLength = 10)
+  knnFit <- train(make.names(label) ~ ., data = traindata, method = 'knn', preProcess = c('center', 'scale'), tuneLength = 10)
   return(knnFit)
 }
 
 confusion.matrix <- function(model, test.data) {
   pred <- predict(model, newdata = test.data)
-  test.data$label <- factor(test.data$label)
+  test.data$label <- factor(make.names(test.data$label))
   cm <- confusionMatrix(pred, test.data$label)
   print(cm$table)
+  print(cm$overall["Accuracy"])
   #return(table(test.data$label, pred))
 }
 
 test <- load.file("https://pages.scinet.utoronto.ca/~afedosee/seeds_dataset2.txt")
+#test <- load.file("https://pages.scinet.utoronto.ca/~afedosee/cars_labeled.csv")
 #print(test)
-#print(test[1])
+#print(as.data.frame(test[2]))
 testknn <- kNN.model(as.data.frame(test[1]))
+#print(testknn)
 testdt <- data.model(as.data.frame(test[1]), "DT")
-print(predict(testdt, newdata = as.data.frame(test[2])))
-print(as.data.frame(test[2])$label)
+#print(predict(testdt, newdata = as.data.frame(test[2])))
+#print(as.data.frame(test[2])$label)
 #print(data.model(as.data.frame(test[1]), "SVM"))
-#confusion.matrix(testknn, as.data.frame(test[2]))
+confusion.matrix(testdt, as.data.frame(test[2]))
